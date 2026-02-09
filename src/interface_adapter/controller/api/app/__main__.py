dddad658ab5d446aaa9839264842659301ee14bf
AgustinMadygraf <agustin.mtto.madygraf@gmail.com>
2026-02-09 14:25:18 -0300

    Remove all test files related to deprecated functionality and unused test cases

--- src/interface_adapter/controller/api/app/__main__.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-
-from src.interface_adapter.controller.api.app.main import app
-from src.interface_adapter.controller.api.app.settings import settings
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=settings.port)
--- src/interface_adapter/controller/cli/__main__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from src.interface_adapter.controller.cli.main import app
-
-if __name__ == "__main__":
-    app()
